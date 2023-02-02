b84881a368c8dbe03e0ced3bbfc83719fb989cb3
Rutvik Lhase <Rutvik.Lhase@genmills.com>
2023-02-02 22:52:09 +0530

    Changed listening port to 9000

--- build/libs/project.js
+++ build/libs/project.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, "localhost", () => {
+server.listen(9000, "localhost", () => {
   console.log("Listening for request");
 });
 
