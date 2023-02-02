8a230a6c934d2e2e820860b6779c2cc5392d4e24
Rutvik Lhase <Rutvik.Lhase@genmills.com>
2023-02-02 22:49:49 +0530

    Added NGINX, deleted workflow, dockerfile

--- project2.js
+++ project2.js
@@ -20,16 +20,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, "localhost", () => {
   console.log("Listening for request");
 });
-
-
-// var http = require('http'); // 1 - Import Node.js core module
-
-// var server = http.createServer(function (req, res) {   // 2 - creating server
-
-//     //handle incomming requests here..
-
-// });
-
-// server.listen(9000); //3 - listen for any incoming requests
-
-// console.log('Node.js web server at port 9000 is running... this one is new though')
